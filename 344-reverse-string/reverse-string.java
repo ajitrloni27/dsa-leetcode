1da334029612a504b8cd7ba3fac58402509f5eee
Ajit <ajitrloni27@gmail.com>
2026-09-08 18:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (73.80%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char temp;
+        for(int i=0; i<s.length/2; i++){      // traverse half of the string
+            temp= s[i];                         // assign 1st character
+            s[i]= s[s.length-1 -i];             // swap 
+            s[s.length-1-i]= temp;              // swap from last to first 
+
+        }
+        
+    }
+}
